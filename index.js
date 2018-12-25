dda37809f7ae94aff6d295422222445b4d93c3f3
zfang <747320213@qq.com>
2018-12-25 15:47:17 +0800

    完成promise功能

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import appPrompt from './components/appPrompt.vue'
 
+let components = [],
+    options = [],
+    instances = []
+
 let componentPopUp = {
   install (Vue) {
     // 注入实例方法
@@ -12,52 +16,81 @@ let componentPopUp = {
     document.body.appendChild(popUp.$mount().$el)
   },
   state: {
-    components: [],
-    options: [],
-    instances: []
+    components,
+    options,
+    instances
   },
   mutations: {
     ADD_POPUP ({component, options}) {
-      componentPopUp.state.components.push(component)
+      components.push(component)
       componentPopUp.state.options.push(options)
     },
-    CANCEL_A_POPUP (componentIndex) {
-      [componentPopUp.state.components, componentPopUp.state.instances, componentPopUp.state.options].forEach((stack) => {
+    REMOVE_A_POPUP () {
+      let arr = [components, instances, options]
+      for (let i = 0; i < arr.length; i++) {
+        let stack = arr[i]
         stack.splice(componentIndex - stack.length, 1) 
-      })
+      }
+    },
+    CLEAR () {
+      let arr = [components, instances, options]
+      for (let i = 0; i < arr.length; i++) {
+        let stack = arr[i]
+        stack.splice(0, stack.length) 
+      }
     }
   },
   actions: {
-    async popUp (component,
-                 options = {
+    async popUp (component, options = {
                     livingTime: undefined,
                     wrapperStyle: undefined,
                     stackContext: '#appPrompt',
                     mountedEventEmiter: undefined,
                     destroyedEventEmiter: undefined
-                  }
-                ) {
+                  } ) {
       componentPopUp.mutations.ADD_POPUP({component, options})
       return new Promise((resolve) => {
         options.mountedEventEmiter = resolve
       })
     },
     async cancel (key) {
-      let methodsSwitch = {
-        Object: function () {
-          componentPopUp.state.components.forEach((element, index) => {
-            if (element === key) componentPopUp.mutations.CANCEL_A_POPUP(index)
-          })
-        },
-        VueComponent: function () {
-          let componentIndex = componentPopUp.state.instances.findIndex(key)
-          componentPopUp.mutations.CANCEL_A_POPUP(componentIndex)
+      if (key) {
+        for (let i = instances.length; i > 0; i--) {
+          const vm = instances[i], component = components[i]
+          if (
+            (key.__proto__.constructor.name === 'VueComponent' && key === vm)
+            || (key === component)
+          ) {
+            return new Promise(resolve => {
+              options[i].destroyedEventEmiter = resolve
+              componentPopUp.mutations.REMOVE_A_POPUP( i )
+            })
+          }
         }
       }
-      methodsSwitch[key.__proto__.constructor]()
-      // return new Promise(resolve => {
-      //   options.destroyedEventEmiter = resolve
-      // })
+      else {
+        return new Promise(resolve => {
+          options[components.length - 1].destroyedEventEmiter = resolve
+          componentPopUp.mutations.REMOVE_A_POPUP(components.length - 1)
+        })
+      }
+    },
+    async cancelAll (VueComponent) {
+      let promiseList = []
+      if (VueComponent) {
+        let componentIndex = components.findIndex(VueComponent)
+        while (componentIndex !== -1) {
+          promiseList.push( componentPopUp.actions.cancel(VueComponent) )
+        }
+      } else {
+        for (let element of options) {
+          promiseList.push(new Promise(resolve => {
+            element.destroyedEventEmiter = resolve
+          }))
+        }
+        componentPopUp.mutations.CLEAR()
+      }
+      return Promise.all(promiseList)
     }
   }
 }
